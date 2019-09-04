4df6b80b0b24cdb9a09320cdc503903129f6b2f1
marcollie <marcoys12@daum.net>
2019-09-04 15:52:57 +0900

    Updates

---
+++ precache-manifest.c78439876399164f3218e9cad2bbfde7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbbcd13cc94d73a67397be9ceedd7f34",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "30eab97e72100de46c3c",
+    "url": "/movie_app/static/css/main.1ec94d34.chunk.css"
+  },
+  {
+    "revision": "e183bcee449302b2a05c",
+    "url": "/movie_app/static/js/2.6196da2f.chunk.js"
+  },
+  {
+    "revision": "30eab97e72100de46c3c",
+    "url": "/movie_app/static/js/main.110778c9.chunk.js"
+  },
+  {
+    "revision": "d179891a5f5e4ba41521",
+    "url": "/movie_app/static/js/runtime~main.76fa3daa.js"
+  }
+]);
